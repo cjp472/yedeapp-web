382358d58ffc087fc89c06d7b376e6665e7102a2
Yede <yedeapp@163.com>
2017-12-16 17:12:24 +0800

    Create books

---
+++ database/migrations/2017_12_16_164103_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->index()->comment('书名');
+            $table->text('brief')->nullable()->comment('描述');
+            $table->text('preface')->nullable()->comment('简介');
+            $table->integer('chapter_count')->default(0)->comment('章数');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
